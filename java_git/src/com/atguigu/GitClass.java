181c2876bc7c2d825993ae80de83f597cc58cd17
yangjun_glb <2635136453@qq.com>
2019-10-21 16:30:04 +0800

    first commit

---
+++ java_git/src/com/atguigu/GitClass.java
@@ -0,0 +1,11 @@
+package com.atguigu;
+
+public class GitClass {
+    public static void main(String[] args) {
+        System.out.println("aaaaa");
+        System.out.println("bbbbb");
+        System.out.println("ccccc");
+        System.out.println("ddddd");
+        System.out.println("eeeee");
+    }
+}
