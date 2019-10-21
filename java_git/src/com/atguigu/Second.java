dd1376a04d1b66dd739c253a3c69a43b3087793e
yangjun_glb <2635136453@qq.com>
2019-10-21 17:52:47 +0800

    test conflict t3

--- java_git/src/com/atguigu/Second.java
+++ java_git/src/com/atguigu/Second.java
@@ -5,9 +5,9 @@ public class Second {
         int i=0;
         //打印输出语句1
         System.out.println(i);//输出0
-        //this is a test
-        int a=3;
-        //this is a end
+        //this is a test111
+        int a=3;//1111
+        //this is a end/111
     }
     public  void  hello() {
         //打印输出hello1
